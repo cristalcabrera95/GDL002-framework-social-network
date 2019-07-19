f7ab3f05fb0843308baca3c47f70f8621b38a00e
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-07-19 09:16:09 -0500

    Updates

---
+++ precache-manifest.6a8eb7a3f0c88a89b734baa8c4c24c9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d292ae4b9e1cb0fb6e2e9c71c9587e81",
+    "url": "/framework-social-network/index.html"
+  },
+  {
+    "revision": "566353f10fc506323668",
+    "url": "/framework-social-network/static/css/main.280725ff.chunk.css"
+  },
+  {
+    "revision": "e0ed43822fb6c59076e8",
+    "url": "/framework-social-network/static/js/2.a6f7c0e5.chunk.js"
+  },
+  {
+    "revision": "566353f10fc506323668",
+    "url": "/framework-social-network/static/js/main.5c72bab7.chunk.js"
+  },
+  {
+    "revision": "ca0e51c1bd7b69e174fc",
+    "url": "/framework-social-network/static/js/runtime~main.2834e1ba.js"
+  }
+]);
